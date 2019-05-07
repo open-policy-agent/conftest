66275f814ce71acc06d8ef150318aa88e5721c63
Gareth Rushgrove <gareth@morethanseven.net>
2019-05-07 10:24:37 +0100

    Remove vendored pkg from oras
    Oras now supports go modules in master, so update to use that. Details in https://github.com/deislabs/oras/issues/98

--- conftest.go
+++ conftest.go
@@ -25,7 +25,7 @@ import (
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
 
-	auth "github.com/instrumenta/conftest/pkg/auth/docker"
+	auth "github.com/deislabs/oras/pkg/auth/docker"
 )
 
 var (
