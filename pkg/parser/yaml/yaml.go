e9778e5ba81c801babbac054905a029db8e9764a
John Calabrese <xchapter7x@gmail.com>
2019-07-31 16:46:57 -0400

    Implement multi-document support within YAML parser
    [#167598039]
    
    Signed-off-by: David Lohle <Proplex@users.noreply.github.com>

--- pkg/parser/yaml/yaml.go
+++ pkg/parser/yaml/yaml.go
@@ -1,7 +1,10 @@
 package yaml
 
 import (
+	"bytes"
 	"fmt"
+	"runtime"
+
 	"github.com/ghodss/yaml"
 )
 
@@ -9,11 +12,49 @@ type Parser struct {
 	FileName string
 }
 
-func (yp *Parser) Unmarshal(p []byte, v interface{}) error {
+func (yp *Parser) separateSubDocuments(data []byte) [][]byte {
+	linebreak := "\n"
+	windowsLineEnding := bytes.Contains(data, []byte("\r\n"))
+	if windowsLineEnding && runtime.GOOS == "windows" {
+		linebreak = "\r\n"
+	}
+	return bytes.Split(data, []byte(linebreak+"---"+linebreak))
+}
+
+func (yp *Parser) unmarshalSingleDocument(p []byte, v interface{}) error {
 	err := yaml.Unmarshal(p, v)
 	if err != nil {
 		return fmt.Errorf("Unable to parse YAML from %s: %s", yp.FileName, err)
 	}
-
 	return nil
+}
+
+func (yp *Parser) unmarshalMultipleDocuments(subDocuments [][]byte, v interface{}) error {
+	var documentStore []interface{}
+	for _, subDocument := range subDocuments {
+		var documentObject interface{}
+		err := yaml.Unmarshal(subDocument, &documentObject)
+		if err != nil {
+			return fmt.Errorf("Unable to parse YAML from %s: %s", yp.FileName, err)
+		}
+		documentStore = append(documentStore, documentObject)
+	}
+
+	yamlConfigBytes, err := yaml.Marshal(documentStore)
+	if err != nil {
+		return fmt.Errorf("Unable to marshal documentStore %v: %s", documentStore, err)
+	}
+	yaml.Unmarshal(yamlConfigBytes, v)
+	if err != nil {
+		return fmt.Errorf("Unable to Unmarshal yamlConfigBytes %s: %s", string(yamlConfigBytes), err)
+	}
+	return nil
+}
+
+func (yp *Parser) Unmarshal(p []byte, v interface{}) error {
+	subDocuments := yp.separateSubDocuments(p)
+	if len(subDocuments) <= 1 {
+		return yp.unmarshalSingleDocument(p, v)
+	}
+	return yp.unmarshalMultipleDocuments(subDocuments, v)
 }
