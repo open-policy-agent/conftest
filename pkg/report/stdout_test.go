9c79b5ac7bba4f381f9f00c0ab0ebd8a5a583a6b
Lennard Eijsackers <lennardeijsackers92@gmail.com>
2019-08-05 11:45:41 +0200

    chore: Remove comments in test

--- pkg/report/stdout_test.go
+++ pkg/report/stdout_test.go
@@ -9,53 +9,6 @@ import (
 	"gotest.tools/assert"
 )
 
-// func TestStdOutReporter_Report(t *testing.T) {
-
-// 	type fields struct {
-// 		logger *log.Logger
-// 		color  aurora.Aurora
-// 	}
-// 	type args struct {
-// 		level    Level
-// 		fileName string
-// 		msg      string
-// 	}
-	// tests := []struct {
-	// 	name string
-	// 	args args
-	// 	exp  string
-	// }{
-	// 	{
-	// 		"Logs warning messages in correct format",
-	// 		args{
-	// 			Warn,
-	// 			"test",
-	// 			"testing warning messages",
-	// 		},
-	// 		"WARN - test - testing warning messages",
-	// 	},
-	// 	{
-	// 		"Logs error messages in correct format",
-	// 		args{
-	// 			Error,
-	// 			"test",
-	// 			"testing error messages",
-	// 		},
-	// 		"FAIL - test - testing error messages",
-	// 	},
-	// }
-// 	for _, tt := range tests {
-// 		t.Run(tt.name, func(t *testing.T) {
-// 			buf := new(bytes.Buffer)
-// 			reporter := NewStdOutReporter(log.New(buf, "", 0), false)
-
-// 			reporter.Report(tt.args.level, tt.args.fileName, tt.args.msg)
-// 			res := strings.TrimSuffix(buf.String(), "\n")
-// 			assert.Equal(t, res, tt.exp)
-// 		})
-// 	}
-// }
-
 func TestStdOutReporter_Report(t *testing.T) {
 	tests := []struct {
 		name   string
