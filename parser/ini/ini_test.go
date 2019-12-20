3364fc9f36b848ccd38707baa6d3198ce5c2b489
KeisukeYamashita <19yamashita15@gmail.com>
2019-12-20 12:01:42 +0900

    Add types for ini file type

--- parser/ini/ini_test.go
+++ parser/ini/ini_test.go
@@ -10,6 +10,8 @@ func TestIniParser(t *testing.T) {
 	Name=name
 	Title=title
 	Visisbility=show/hide
+	Delay=10
+
 
 	[Navigation Controls]
 	OnNext=node path
@@ -30,5 +32,28 @@ func TestIniParser(t *testing.T) {
 	item := inputMap["Local Varaibles"]
 	if len(item.(map[string]interface{})) <= 0 {
 		t.Error("there should be at least one item defined in the parsed file, but none found")
+	}
+}
+
+func TestConvertTypes(t *testing.T) {
+	testTable := []struct {
+		name           string
+		input          map[string]string
+		expectedOutput interface{}
+	}{
+		{"Test number literal", map[string]string{"test": "3.0"}, 3.0},
+		{"Test string literal", map[string]string{"test": "conftest"}, "conftest"},
+		{"Test boolean literal", map[string]string{"test": "true"}, true},
+	}
+
+	for _, testUnit := range testTable {
+		t.Run(testUnit.name, func(t *testing.T) {
+			val := convertKeyTypes(testUnit.input)
+			for _, v := range val {
+				if v != testUnit.expectedOutput {
+					t.Fatalf("convert type got wrong value %v want %v", v, testUnit.expectedOutput)
+				}
+			}
+		})
 	}
 }
