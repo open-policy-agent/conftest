84f6c389c77cade4104129e50338170ce77cc033
boranx <boran.seref@gmail.com>
2020-01-20 21:01:35 +0100

    add combine flag for parse command

--- internal/commands/parse.go
+++ internal/commands/parse.go
@@ -24,23 +24,26 @@ the '--input' flag can be used to explicitly set the input type, e.g.:
 
 See the documentation of the '--input' flag for the supported input types.
 `
+
 // NewParseCommand creates a parse command.
 // This command can be used for printing structured inputs from unstructured configuration inputs.
 func NewParseCommand(ctx context.Context) *cobra.Command {
 	cmd := cobra.Command{
 		Use:   "parse [file...]",
 		Short: "Print out structured data from your input files",
-		Long: parseDesc,
+		Long:  parseDesc,
 		PreRunE: func(cmd *cobra.Command, args []string) error {
-			err := viper.BindPFlag("input", cmd.Flags().Lookup("input"))
-			if err != nil {
-				return fmt.Errorf("failed to bind argument: %w", err)
+			flagNames := []string{"input", "combine"}
+			for _, name := range flagNames {
+				if err := viper.BindPFlag(name, cmd.Flags().Lookup(name)); err != nil {
+					return fmt.Errorf("bind flag: %w", err)
+				}
 			}
 
 			return nil
 		},
 		RunE: func(cmd *cobra.Command, fileList []string) error {
-			out, err := parseInput(ctx, viper.GetString("input"), fileList)
+			out, err := parseInput(ctx, viper.GetString("input"), viper.GetBool("combine"), fileList)
 			if err != nil {
 				return fmt.Errorf("failed during parser process: %w", err)
 			}
@@ -50,17 +53,18 @@ func NewParseCommand(ctx context.Context) *cobra.Command {
 		},
 	}
 
+	cmd.Flags().BoolP("combine", "", false, "combine all given config files to be evaluated together")
 	cmd.Flags().StringP("input", "i", "", fmt.Sprintf("input type for given source, especially useful when using conftest with stdin, valid options are: %s", parser.ValidInputs()))
 	return &cmd
 }
 
-func parseInput(ctx context.Context, input string, fileList []string) (string, error) {
+func parseInput(ctx context.Context, input string, combine bool, fileList []string) (string, error) {
 	configurations, err := parser.GetConfigurations(ctx, input, fileList)
 	if err != nil {
 		return "", fmt.Errorf("calling the parser method: %w", err)
 	}
 
-	parsedConfigurations, err := parseConfigurations(configurations)
+	parsedConfigurations, err := parseConfigurations(configurations, combine)
 	if err != nil {
 		return "", fmt.Errorf("parsing configs: %w", err)
 	}
@@ -69,27 +73,43 @@ func parseInput(ctx context.Context, input string, fileList []string) (string, e
 
 }
 
-func parseConfigurations(configurations map[string]interface{}) (string, error) {
+func parseConfigurations(configurations map[string]interface{}, combine bool) (string, error) {
 	var output string
-	for filename, config := range configurations {
-		out, err := json.Marshal(config)
+	if combine {
+		content, err := marshal(configurations)
 		if err != nil {
 			return "", fmt.Errorf("marshal output to json: %w", err)
 		}
 
-		var prettyJSON bytes.Buffer
-		if err = json.Indent(&prettyJSON, out, "", "\t"); err != nil {
-			return "", fmt.Errorf("indentation: %w", err)
-		}
+		output = strings.Replace(output+"\n"+content, "\\r", "", -1)
+	} else {
+		for filename, config := range configurations {
+			content, err := marshal(config)
+			if err != nil {
+				return "", fmt.Errorf("marshal output to json: %w", err)
+			}
 
-		if _, err := prettyJSON.WriteString("\n"); err != nil {
-			return "", fmt.Errorf("adding line break: %w", err)
+			output = strings.Replace(output+filename+"\n"+content, "\\r", "", -1)
 		}
-
-		output = output + filename + "\n"
-		output = output + prettyJSON.String()
-		output = strings.Replace(output, "\\r", "", -1)
 	}
 
 	return output, nil
+}
+
+func marshal(in interface{}) (string, error) {
+	out, err := json.Marshal(in)
+	if err != nil {
+		return "", fmt.Errorf("marshal output to json: %w", err)
+	}
+
+	var prettyJSON bytes.Buffer
+	if err = json.Indent(&prettyJSON, out, "", "\t"); err != nil {
+		return "", fmt.Errorf("indentation: %w", err)
+	}
+
+	if _, err := prettyJSON.WriteString("\n"); err != nil {
+		return "", fmt.Errorf("adding line break: %w", err)
+	}
+
+	return prettyJSON.String(), nil
 }
--- internal/commands/parse_test.go
+++ internal/commands/parse_test.go
@@ -6,6 +6,7 @@ import (
 )
 
 func TestParse_ByDefault_AddsIndentationAndNewline(t *testing.T) {
+	combine := false
 	configurations := make(map[string]interface{})
 
 	config := struct {
@@ -17,7 +18,7 @@ func TestParse_ByDefault_AddsIndentationAndNewline(t *testing.T) {
 	const expectedFileName = "file.json"
 	configurations[expectedFileName] = config
 
-	actual, err := parseConfigurations(configurations)
+	actual, err := parseConfigurations(configurations, combine)
 	if err != nil {
 		t.Fatalf("parsing configs: %s", err)
 	}
@@ -34,5 +35,44 @@ func TestParse_ByDefault_AddsIndentationAndNewline(t *testing.T) {
 
 	if !strings.Contains(actual, expectedFileName) {
 		t.Errorf("unexpected parsed filename. expected %v actual %v", expected, actual)
+	}
+}
+
+func TestParse_MultiFileCombineFlag(t *testing.T) {
+	combine := true
+	configurations := make(map[string]interface{})
+
+	config := struct {
+		Sut string
+	}{
+		Sut: "test",
+	}
+
+	config2 := struct {
+		Foo string
+	}{
+		Foo: "bar",
+	}
+
+	configurations["file1.json"] = config
+	configurations["file2.json"] = config2
+
+	actual, err := parseConfigurations(configurations, combine)
+	if err != nil {
+		t.Fatalf("parsing configs: %s", err)
+	}
+
+	expected := `{
+	"file1.json": {
+		"Sut": "test"
+	},
+	"file2.json": {
+		"Foo": "bar"
+	}
+}
+`
+
+	if !strings.Contains(actual, expected) {
+		t.Errorf("unexpected parsed config. expected %v actual %v", expected, actual)
 	}
 }
