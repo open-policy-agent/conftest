3e5e03c9371768cb96233e84581008ae5dd70ccc
John Reese <john@reese.dev>
2020-09-22 13:33:50 -0400

    Remove custom HCL2 conversion (#403)
    Signed-off-by: John Reese <john@reese.dev>

--- parser/hcl2/convert_test.go
+++ parser/hcl2/convert_test.go
@@ -1,11 +1,9 @@
 package hcl2
 
 import (
-	"encoding/json"
 	"testing"
 
-	"github.com/hashicorp/hcl/v2"
-	"github.com/hashicorp/hcl/v2/hclsyntax"
+	"github.com/tmccombs/hcl2json/convert"
 )
 
 // This file is mostly attributed to https://github.com/tmccombs/hcl2json
@@ -45,34 +43,34 @@ resource "aws_elastic_beanstalk_environment" "example" {
   }`
 
 const outputa = `{
-	"resource": {
-		"aws_elastic_beanstalk_environment": {
-			"example": {
-				"application": "testing",
-				"dynamic": {
-					"setting": {
-						"content": {
-							"cond": "${test3 \u003e 2 ? 1: 0}",
-							"heredoc": "This is a heredoc template.\nIt references ${local.other.3}\n",
-							"heredoc2": "\t\t\tAnother heredoc, that\n\t\t\tdoesn't remove indentation\n\t\t\t${local.other.3}\n\t\t\t%{if true ? false : true}\"gotcha\"\\n%{else}4%{endif}\n",
-							"loop": "This has a for loop: %{for x in local.arr}x,%{endfor}",
-							"name": "${setting.key}",
-							"namespace": "aws:elasticbeanstalk:application:environment",
-							"simple": "${4 - 2}",
-							"value": "${setting.value}"
-						},
-						"for_each": "${data.consul_key_prefix.environment.var}"
-					}
-				},
-				"name": "test_environment",
-				"setting": {
-					"name": "MinSize",
-					"namespace": "aws:autoscaling:asg",
-					"value": "1"
-				}
-			}
-		}
-	}
+    "resource": {
+        "aws_elastic_beanstalk_environment": {
+            "example": {
+                "application": "testing",
+                "dynamic": {
+                    "setting": {
+                        "content": {
+                            "cond": "${test3 \u003e 2 ? 1: 0}",
+                            "heredoc": "This is a heredoc template.\nIt references ${local.other.3}\n",
+                            "heredoc2": "\t\t\tAnother heredoc, that\n\t\t\tdoesn't remove indentation\n\t\t\t${local.other.3}\n\t\t\t%{if true ? false : true}\"gotcha\"\\n%{else}4%{endif}\n",
+                            "loop": "This has a for loop: %{for x in local.arr}x,%{endfor}",
+                            "name": "${setting.key}",
+                            "namespace": "aws:elasticbeanstalk:application:environment",
+                            "simple": "${4 - 2}",
+                            "value": "${setting.value}"
+                        },
+                        "for_each": "${data.consul_key_prefix.environment.var}"
+                    }
+                },
+                "name": "test_environment",
+                "setting": {
+                    "name": "MinSize",
+                    "namespace": "aws:autoscaling:asg",
+                    "value": "1"
+                }
+            }
+        }
+    }
 }`
 
 const inputb = `
@@ -83,12 +81,12 @@ provider "aws" {
 `
 
 const outputb = `{
-	"provider": {
-		"aws": {
-			"alias": "one",
-			"version": "=2.46.0"
-		}
-	}
+    "provider": {
+        "aws": {
+            "alias": "one",
+            "version": "=2.46.0"
+        }
+    }
 }`
 
 const inputc = `
@@ -103,18 +101,18 @@ provider "aws" {
 `
 
 const outputc = `{
-	"provider": {
-		"aws": [
-			{
-				"alias": "one",
-				"version": "=2.46.0"
-			},
-			{
-				"alias": "two",
-				"version": "=2.47.0"
-			}
-		]
-	}
+    "provider": {
+        "aws": [
+            {
+                "alias": "one",
+                "version": "=2.46.0"
+            },
+            {
+                "alias": "two",
+                "version": "=2.47.0"
+            }
+        ]
+    }
 }`
 
 // Test that conversion works as expected
@@ -129,22 +127,13 @@ func TestConversion(t *testing.T) {
 	}
 	for name, tc := range testTable {
 		bytes := []byte(tc.input)
-		conf, diags := hclsyntax.ParseConfig(bytes, "test", hcl.Pos{Byte: 0, Line: 1, Column: 1})
-		if diags.HasErrors() {
-			t.Errorf("Failed to parse config: %v", diags)
-		}
-		converted, err := convertFile(conf)
 
+		json, err := convert.Bytes(bytes, "", convert.Options{})
 		if err != nil {
-			t.Errorf("Unable to convert from hcl: %v", err)
+			t.Fatal("convert bytes:", err)
 		}
 
-		jb, err := json.MarshalIndent(converted, "", "\t")
-		if err != nil {
-			t.Errorf("Failed to serialize to json: %v", err)
-		}
-		computedJSON := string(jb)
-
+		computedJSON := string(json)
 		if computedJSON != tc.output {
 			t.Errorf("For test %s\nExpected:\n%s\n\nGot:\n%s", name, tc.output, computedJSON)
 		}
