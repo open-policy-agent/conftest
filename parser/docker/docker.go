c58d2127d0b94a1df2273694c2d65b859f4495ac
Lennard Eijsackers <lennardeijsackers92@gmail.com>
2021-03-04 21:17:53 +0100

    Support a build stage indicator in the Docker parser.
    Signed-off-by: Lennard Eijsackers <lennardeijsackers92@gmail.com>

--- parser/docker/docker.go
+++ parser/docker/docker.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 
 	"github.com/moby/buildkit/frontend/dockerfile/parser"
+	"github.com/moby/buildkit/frontend/dockerfile/instructions"
 )
 
 // Parser is a Dockerfile parser.
@@ -13,11 +14,18 @@ type Parser struct{}
 
 // Command represents a command in a Dockerfile.
 type Command struct {
-	Cmd    string   // lowercased command name (ex: `from`)
-	SubCmd string   // for ONBUILD only this holds the sub-command
-	JSON   bool     // whether the value is written in json form
-	Flags  []string // Any flags such as `--from=...` for `COPY`.
-	Value  []string // The contents of the command (ex: `ubuntu:xenial`)
+	// lowercased command name (ex: `from`)
+	Cmd    string   
+	// for ONBUILD only this holds the sub-command
+	SubCmd string   
+	// whether the value is written in json form
+	JSON   bool     	
+	// Any flags such as `--from=...` for `COPY`.
+	Flags  []string
+	// The contents of the command (ex: `ubuntu:xenial`)
+	Value  []string
+	// Stage indicates which stage the command is found in a multistage docker build
+	Stage int
 }
 
 // Unmarshal unmarshals Dockerfiles
@@ -28,13 +36,23 @@ func (dp *Parser) Unmarshal(p []byte, v interface{}) error {
 		return fmt.Errorf("parse dockerfile: %w", err)
 	}
 
-	// Code attributed to https://github.com/asottile/dockerfile
-	// TODO: Just import the package
 	var commands []Command
+	var stages []*instructions.Stage
 	for _, child := range res.AST.Children {
+		instr, err := instructions.ParseInstruction(child)
+		if err != nil {
+			return fmt.Errorf("process dockerfile instructions: %w", err)
+		}
+
+		stage, ok := instr.(*instructions.Stage)
+		if ok {
+			stages = append(stages, stage)
+		}
+
 		cmd := Command{
 			Cmd:   child.Value,
 			Flags: child.Flags,
+			Stage: currentStage(stages),
 		}
 
 		if child.Next != nil && len(child.Next.Children) > 0 {
@@ -63,4 +81,14 @@ func (dp *Parser) Unmarshal(p []byte, v interface{}) error {
 	}
 
 	return nil
+}
+
+// Return the index of the stages. If no stages are present,
+// we set the index to zero.
+func currentStage(stages []*instructions.Stage) int {
+	if len(stages) == 0 {
+		return 0
+	}
+
+	return len(stages) - 1
 }
