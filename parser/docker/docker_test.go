091d6d574f7f4feaed1c6b51b7a7c15b3ea7eda2
Lennard Eijsackers <lennardeijsackers92@gmail.com>
2021-03-07 19:30:17 +0100

    Support a build stage indicator in the Docker parser. (#499)
    Signed-off-by: Lennard Eijsackers <lennardeijsackers92@gmail.com>

--- parser/docker/docker_test.go
+++ parser/docker/docker_test.go
@@ -31,3 +31,42 @@ RUN echo hello`
 		t.Errorf("first Docker command should be '%v', was '%v'", expected, actual)
 	}
 }
+
+func TestParser_Unmarshal_Multistage(t *testing.T) {
+	parser := Parser{}
+
+	sample := `FROM golang:1.13-alpine as base
+COPY go.mod .
+COPY go.sum .
+RUN go mod download
+
+COPY . .
+	
+FROM base as builder
+RUN go build -o conftest`
+
+	var input interface{}
+	if err := parser.Unmarshal([]byte(sample), &input); err != nil {
+		t.Fatalf("parser should not have thrown an error: %v", err)
+	}
+
+	if input == nil {
+		t.Error("there should be information parsed but its nil")
+	}
+
+	dockerFile := input.([]interface{})[0]
+	commands := dockerFile.([]interface{})
+	
+	cmd := commands[1]
+	stage := cmd.(map[string]interface{})["Stage"].(float64)
+	if stage != 0 {
+		t.Errorf("expected command to be in stage 0, not stage: %v", stage)
+	}
+
+	cmd = commands[6]
+	stage = cmd.(map[string]interface{})["Stage"].(float64)
+	if stage != 1 {
+		t.Errorf("expected command to be in stage 1, not stage: %v", stage)
+	}
+	t.Logf("%v", commands)
+}
