cadee70442d6160eaa54cac8c644d35eddc65b30
Greggory Rothmeier <greggroth@github.com>
2022-04-06 12:48:42 -0700

    Add parser for JSONC documents (#691)
    JSON with Comments is used in the Devcontainer configuration
    (https://code.visualstudio.com/docs/remote/devcontainerjson-reference).
    
    Signed-off-by: Greggory Rothmeier <greggroth@github.com>

---
+++ parser/jsonc/jsonc.go
@@ -0,0 +1,19 @@
+package jsonc
+
+import (
+	"fmt"
+
+	"muzzammil.xyz/jsonc"
+)
+
+// Parser is a JSON parser.
+type Parser struct{}
+
+// Unmarshal unmarshals JSON files.
+func (p *Parser) Unmarshal(data []byte, v interface{}) error {
+	if err := jsonc.Unmarshal(data, v); err != nil {
+		return fmt.Errorf("unmarshal jsonc: %w", err)
+	}
+
+	return nil
+}
---
+++ parser/jsonc/jsonc_test.go
@@ -0,0 +1,43 @@
+package jsonc
+
+import (
+	"testing"
+)
+
+func TestJSONParser(t *testing.T) {
+	parser := &Parser{}
+	sample := `{
+  "name": "conftest-example", // Ignore comments
+  "version": "1.0.0",
+  "description": "An example of testing Typescript code with Open Policy Agent",
+  "main": "pod.js",
+  "scripts": {
+    "test": "echo \"Error: no test specified\" && exit 1"
+  },
+  /* Like this */
+  "author": "",
+  "license": "ISC",
+  "dependencies": {
+    "js-yaml": "^3.13.1",
+    "kubernetes-types": "^1.13.0-beta.1"
+  },
+  "devDependencies": {
+    "ts-node": "^8.1.0",
+    "typescript": "^3.4.5"
+  }
+}`
+
+	var input interface{}
+	if err := parser.Unmarshal([]byte(sample), &input); err != nil {
+		t.Fatalf("parser should not have thrown an error: %v", err)
+	}
+
+	if input == nil {
+		t.Fatalf("there should be information parsed but its nil")
+	}
+
+	inputMap := input.(map[string]interface{})
+	if len(inputMap) == 0 {
+		t.Error("there should be at least one item defined in the parsed file, but none found")
+	}
+}
