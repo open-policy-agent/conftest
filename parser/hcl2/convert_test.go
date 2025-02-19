259aaede2aae8b3acac73f40b776b6d743c47bcb
James Alseth <james@jalseth.me>
2025-02-18 22:43:56 -0800

    deps: Bump hcl2json to v0.6.5
    Signed-off-by: James Alseth <james@jalseth.me>

--- parser/hcl2/convert_test.go
+++ parser/hcl2/convert_test.go
@@ -1,146 +1,150 @@
 package hcl2
 
 import (
-	"bytes"
-	"encoding/json"
 	"testing"
 
-	"github.com/tmccombs/hcl2json/convert"
+	"github.com/google/go-cmp/cmp"
 )
 
-const inputa = `
-resource "aws_elastic_beanstalk_environment" "example" {
-	name        = "test_environment"
-	application = "testing"
+func TestHCL2(t *testing.T) {
 
-	setting {
-	  namespace = "aws:autoscaling:asg"
-	  name      = "MinSize"
-	  value     = "1"
-	}
-
-	dynamic "setting" {
-	  for_each = data.consul_key_prefix.environment.var
-	  content {
-		heredoc = <<-EOF
-		This is a heredoc template.
-		It references ${local.other.3}
-		EOF
-		simple = "${4 - 2}"
-		cond = test3 > 2 ? 1: 0
-		heredoc2 = <<EOF
-			Another heredoc, that
-			doesn't remove indentation
-			${local.other.3}
-			%{if true ? false : true}"gotcha"\n%{else}4%{endif}
-		EOF
-		loop = "This has a for loop: %{for x in local.arr}x,%{endfor}"
-		namespace = "aws:elasticbeanstalk:application:environment"
-		name      = setting.key
-		value     = setting.value
-	  }
-	}
-  }`
-
-const outputa = `{
-    "resource": {
-        "aws_elastic_beanstalk_environment": {
-            "example": {
-                "application": "testing",
-                "dynamic": {
-                    "setting": {
-                        "content": {
-                            "cond": "${test3 \u003e 2 ? 1: 0}",
-                            "heredoc": "This is a heredoc template.\nIt references ${local.other.3}\n",
-                            "heredoc2": "\t\t\tAnother heredoc, that\n\t\t\tdoesn't remove indentation\n\t\t\t${local.other.3}\n\t\t\t%{if true ? false : true}\"gotcha\"\\n%{else}4%{endif}\n",
-                            "loop": "This has a for loop: %{for x in local.arr}x,%{endfor}",
-                            "name": "${setting.key}",
-                            "namespace": "aws:elasticbeanstalk:application:environment",
-                            "simple": "${4 - 2}",
-                            "value": "${setting.value}"
-                        },
-                        "for_each": "${data.consul_key_prefix.environment.var}"
-                    }
-                },
-                "name": "test_environment",
-                "setting": {
-                    "name": "MinSize",
-                    "namespace": "aws:autoscaling:asg",
-                    "value": "1"
-                }
-            }
-        }
-    }
-}`
-
-const inputb = `
-provider "aws" {
-    version             = "=2.46.0"
-    alias                  = "one"
-}
-`
-
-const outputb = `{
-    "provider": {
-        "aws": {
-            "alias": "one",
-            "version": "=2.46.0"
-        }
-    }
-}`
-
-const inputc = `
-provider "aws" {
-    version             = "=2.46.0"
-    alias                  = "one"
-}
-provider "aws" {
-    version             = "=2.47.0"
-    alias                  = "two"
-}
-`
-
-const outputc = `{
-    "provider": {
-        "aws": [
-            {
-                "alias": "one",
-                "version": "=2.46.0"
-            },
-            {
-                "alias": "two",
-                "version": "=2.47.0"
-            }
-        ]
-    }
-}`
-
-func TestConversion(t *testing.T) {
-	testTable := map[string]struct {
-		input  string
-		output string
+	tests := []struct {
+		desc  string
+		input string
+		want  map[string]any
 	}{
-		"simple-resources": {input: inputa, output: outputa},
-		"single-provider":  {input: inputb, output: outputb},
-		"two-providers":    {input: inputc, output: outputc},
+		{
+			desc: "simple-resources",
+			input: `
+resource "aws_elastic_beanstalk_environment" "example" {
+  name        = "test_environment"
+  application = "testing"
+
+  setting {
+    namespace = "aws:autoscaling:asg"
+    name      = "MinSize"
+    value     = "1"
+  }
+
+  dynamic "setting" {
+    for_each = data.consul_key_prefix.environment.var
+    content {
+      heredoc = <<-EOF
+        This is a heredoc template.
+        It references ${local.other.3}
+        EOF
+      simple = "${4 - 2}"
+      cond = test3 > 2 ? 1: 0
+      heredoc2 = <<EOF
+        Another heredoc, that
+        doesn't remove indentation
+        ${local.other.3}
+        %{if true ? false : true}"gotcha"\n%{else}4%{endif}
+      EOF
+      loop = "This has a for loop: %{for x in local.arr}x,%{endfor}"
+      namespace = "aws:elasticbeanstalk:application:environment"
+      name      = setting.key
+      value     = setting.value
+    }
+  }
+}`,
+			want: map[string]any{
+				"resource": map[string]any{
+					"aws_elastic_beanstalk_environment": map[string]any{
+						"example": []any{
+							map[string]any{
+								"name":        "test_environment",
+								"application": "testing",
+								"setting": []any{
+									map[string]any{
+										"name":      "MinSize",
+										"namespace": "aws:autoscaling:asg",
+										"value":     "1",
+									},
+								},
+								"dynamic": map[string]any{
+									"setting": []any{
+										map[string]any{
+											"for_each": "${data.consul_key_prefix.environment.var}",
+											"content": []any{
+												map[string]any{
+													"cond":      "${test3 \u003e 2 ? 1: 0}",
+													"heredoc":   "This is a heredoc template.\nIt references ${local.other.3}\n",
+													"heredoc2":  "        Another heredoc, that\n        doesn't remove indentation\n        ${local.other.3}\n        %{if true ? false : true}\"gotcha\"\\n%{else}4%{endif}\n",
+													"loop":      "This has a for loop: %{for x in local.arr}x,%{endfor}",
+													"name":      "${setting.key}",
+													"namespace": "aws:elasticbeanstalk:application:environment",
+													"simple":    "${4 - 2}",
+													"value":     "${setting.value}",
+												},
+											},
+										},
+									},
+								},
+							},
+						},
+					},
+				},
+			},
+		},
+		{
+			desc: "single-provider",
+			input: `
+provider "aws" {
+  version = "=2.46.0"
+  alias   = "one"
+}
+`,
+			want: map[string]any{
+				"provider": map[string]any{
+					"aws": []any{
+						map[string]any{
+							"alias":   "one",
+							"version": "=2.46.0",
+						},
+					},
+				},
+			},
+		},
+		{
+			desc: "multiple-providers",
+			input: `
+provider "aws" {
+  version = "=2.46.0"
+  alias   = "one"
+}
+provider "aws" {
+  version = "=2.47.0"
+  alias   = "two"
+}
+`,
+			want: map[string]any{
+				"provider": map[string]any{
+					"aws": []any{
+						map[string]any{
+							"alias":   "one",
+							"version": "=2.46.0",
+						},
+						map[string]any{
+							"alias":   "two",
+							"version": "=2.47.0",
+						},
+					},
+				},
+			},
+		},
 	}
 
-	for name, tc := range testTable {
-		testInput := []byte(tc.input)
-
-		convertedInput, err := convert.Bytes(testInput, "", convert.Options{})
-		if err != nil {
-			t.Fatal("convert bytes:", err)
-		}
-
-		var indented bytes.Buffer
-		if err := json.Indent(&indented, convertedInput, "", "    "); err != nil {
-			t.Fatal("Failed to indent file:", err)
-		}
-
-		computedJSON := indented.String()
-		if computedJSON != tc.output {
-			t.Errorf("For test %s\nExpected:\n%s\n\nGot:\n%s", name, tc.output, computedJSON)
-		}
+	p := Parser{}
+	for _, tc := range tests {
+		t.Run(tc.desc, func(t *testing.T) {
+			var got map[string]any
+			if err := p.Unmarshal([]byte(tc.input), &got); err != nil {
+				t.Fatalf("Unmarshal: unexpected error %v", err)
+			}
+			if diff := cmp.Diff(got, tc.want); diff != "" {
+				t.Errorf("HCL2 produced unexpected diff (-want,+got):\n%s", diff)
+			}
+		})
 	}
 }
