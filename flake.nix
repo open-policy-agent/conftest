58ace24522e68fe7c603ad645c4b2fbeb5ddecdb
James Alseth <james@jalseth.me>
2025-10-15 17:36:32 -0400

    build: Add Nix devshell (#1186)
    This allows developers to enter a known-working development environment with
    "nix develop". This includes all dependencies needed to run the targets in
    the Makefile.
    
    Signed-off-by: James Alseth <james@jalseth.me>

---
+++ flake.nix
@@ -0,0 +1,31 @@
+{
+  description = "conftest";
+
+  inputs = {
+    nixpkgs = {
+      url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    };
+    flake-utils = {
+      url = "github:numtide/flake-utils";
+    };
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = nixpkgs.legacyPackages.${system}; in
+      {
+        devShell = pkgs.mkShell rec {
+          packages = with pkgs; [
+            bats
+            docker
+            go
+            golangci-lint
+            gnumake
+            pre-commit
+            regal
+          ];
+        };
+      }
+    );
+
+}
