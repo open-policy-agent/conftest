199e7368f2cbaf0a0aec30b434943c1661e4d969
Rylen Anil <rylen.anil@gmail.com>
2026-08-09 05:05:56 +0000

    fix: remove redundant loop variable copy
    Signed-off-by: Rylen Anil <rylen.anil@gmail.com>

--- runner/test_test.go
+++ runner/test_test.go
@@ -35,7 +35,6 @@ func TestRenameStdinConfiguration(t *testing.T) {
 	}
 
 	for _, tt := range tests {
-		tt := tt
 		t.Run(tt.name, func(t *testing.T) {
 			t.Parallel()
 			renameStdinConfiguration(tt.configs, tt.stdinFilename)
